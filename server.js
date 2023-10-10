855da253ff2c6b0e23e48a2d46b829c1a26ea1c0
Umar Shaikh <umarmukhtar9823077@gmail.com>
2023-10-10 11:27:12 +0530

    update commit

--- server.js
+++ server.js
@@ -2,21 +2,20 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const TwitterStrategy = require('passport-twitter').Strategy;
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Middleware
 app.use(express.static('public'));
-
-// Session middleware
 app.use(session({ secret: 'your_secret_key_here', resave: true, saveUninitialized: true }));
-
-// Initialize Passport
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Replace these with your Twitter API credentials
 const TWITTER_CONSUMER_KEY = 'UXU3c0tVTUZ0NFNLMjBiV2t5NWM6MTpjaQ';
-const TWITTER_CONSUMER_SECRET = '4DrQo7Un_2gsuE5MdUBbf2_DSIn_2sWV7Ft-DJYiYguVFPmOQ1';
+const TWITTER_CONSUMER_SECRET = '9Tqhfk0PcMvowfzLZgZPTCfb1OdBjHzFRx4ntpr8W8D6giqvJX';
 
 // Passport configuration
 passport.use(
@@ -24,34 +23,38 @@ passport.use(
     {
       consumerKey: TWITTER_CONSUMER_KEY,
       consumerSecret: TWITTER_CONSUMER_SECRET,
-      callbackURL: 'http://localhost:5501/auth/twitter/callback/', // Change to your callback URL
+      callbackURL: 'http://localhost:5501/auth/twitter/callback',
     },
     (token, tokenSecret, profile, done) => {
-      // Handle user authentication here
+      // Here, you would typically perform user authentication and store user data in your database.
+      // For this example, we'll simply log the user's Twitter profile.
+      console.log(profile);
       return done(null, profile);
     }
   )
 );
 
-// Serialize user to the session
 passport.serializeUser((user, done) => {
   done(null, user);
 });
 
-// Deserialize user from the session
 passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Home Page');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/auth/twitter', passport.authenticate('twitter'));
+app.post('/auth/twitter/login', passport.authenticate('twitter'), (req, res) => {
+  // This route is for processing the login form. It will redirect to Twitter for authentication.
+  // After successful authentication, it will redirect back to your callback URL.
+  // You can handle any necessary post-authentication logic here.
+});
 
 app.get(
-  '/auth/twitter/callback/',
+  '/auth/twitter/callback',
   passport.authenticate('twitter', { failureRedirect: '/' }),
   (req, res) => {
     // Successful authentication, you can redirect or respond as needed.
